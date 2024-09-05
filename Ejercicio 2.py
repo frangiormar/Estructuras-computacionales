4b2061e766a15f0963b332f08c9ed13c3d2d5398
frangiormar <frangiormar@gmail.com>
2024-09-05 11:38:44 -0500

    Ejercicio 3.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,11 +1,11 @@
 
 print("Ingresa un nuemero entero ");
 n = int(input())
-raiz = pow(n, 0.5)
+raiz = pow(n, 0.5)        #Saca la raiz cuadrada del numero entero ingresado
 
-dif = raiz - int(raiz)
+dif = raiz - int(raiz)        #Guarda la parte real de la raiz calculada, en dif
 
-if dif == 0:
-       print("Entero"); 
+if dif == 0:        #Evalua si la raiz tiene parte real direfente de 0, en otras palabras, si es una exacta o decimal 
+       print("Es potencia cuadrada de " , raiz); 
 else:
-       print("real");
+       print("No es potencia cuadrada de un entero");
