1605ff0811988d617c4abcad4a896c9fe5b37f83
frangiormar <frangiormar@gmail.com>
2024-09-06 11:58:13 -0500

    Ejercicio 5.py

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -4,14 +4,14 @@ n = int(input())
 print("Ingresa una base menor a 10");
 b = int(input())
 resu = 0
-while(b<n):
-    x = n%b
-    pro = x *(10**c) 
-    c = c + 1
-    resu =resu + pro  
-    n = n/b
-    n = int(n)
+while(b<n): #Define el ciclo que va a seguir para cambiarlo de base, que va hasta que b sea mayor que n
+    x = n%b     #Extrae el residuo de dividir n por a base
+    pro = x *(10**c)     #Multiplica el residuo por una potencia de 10 para de este modo guardar lo mas adelante
+    c = c + 1     #Aumenta la potenia de 10 de 1 en 1 paraque los resultadosse vallan guardando en orden
+    resu =resu + pro   #Es la varable que guarda el resultado de la conversion 
+    n = n/b  
+    n = int(n)  #Se re define n omo la parte entera de dividiro por b para que de este modo se puedan haer las dvisiones consecutivas
     
-resu = resu + (n*(10**c))
+resu = resu + (n*(10**c)) #Como el ciclo para cuando n es menor que b, por lo que no se puede dividir, se agrega el dijto de este ultimo valor que toma n al resultado
 print(resu);   
     
