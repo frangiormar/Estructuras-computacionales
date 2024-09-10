30380e50e6a62fa1193f5f53931329b59a780e71
frangiormar <frangiormar@gmail.com>
2024-09-09 21:19:09 -0500

    comentarios

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -10,7 +10,7 @@ while(b<n): #Define el ciclo que va a seguir para cambiarlo de base, que va hast
     c = c + 1     #Aumenta la potenia de 10 de 1 en 1 paraque los resultadosse vallan guardando en orden
     resu =resu + pro   #Es la varable que guarda el resultado de la conversion 
     n = n/b  
-    n = int(n)  #Se re define n omo la parte entera de dividiro por b para que de este modo se puedan haer las dvisiones consecutivas
+    n = int(n)  #Se re define n como la parte entera de dividiro por b para que de este modo se puedan haer las dvisiones consecutivas
     
 resu = resu + (n*(10**c)) #Como el ciclo para cuando n es menor que b, por lo que no se puede dividir, se agrega el dijto de este ultimo valor que toma n al resultado
 print(resu);   
